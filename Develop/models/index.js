03027b99f5cd3617062c0d0aed6335c7c497a51a
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2023-01-09 18:10:46 -0800

    models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -9,8 +9,26 @@ const ProductTag = require('./ProductTag');
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  //Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  //Define an alias for when data is retrieved
+  as: 'product_tag'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  //Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  //Define an alias for when data is retrieved
+  as: 'product_tag'
+});
 
 module.exports = {
   Product,
